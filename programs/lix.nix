0747e6e4fe9c8e1f8621b3c2c59d22f83ae890ec
blokyk <courvoisier.zoe@gmail.com>
2026-02-25 03:05:03 +0100

    nom: downgrade to v2.1.6 to avoid json format mismatch

--- programs/lix.nix
+++ programs/lix.nix
@@ -12,6 +12,17 @@
         nix-eval-jobs
         nix-fast-build
         colmena;
+
+      # we need to use an older version of nix-output-monitor, because
+      # since 2.1.7 it adapts to cppnix's slightly breaking json format,
+      # which lix doesn't use
+      nix-output-monitor = prev.nix-output-monitor.overrideAttrs {
+        version = "2.1.6";
+        src = pkgs.fetchzip {
+          url = "https://code.maralorn.de/maralorn/nix-output-monitor/archive/v2.1.6.tar.gz";
+          sha256 = "sha256-YfxFcGD9U7RzctnTRUQX1Nsz2EtiDIUGpz2nTo0OSWw=";
+        };
+      };
     })
   ];
 }
