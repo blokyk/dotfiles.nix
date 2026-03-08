9ee9094d1db5aa4f838223685496cb9cd2deb776
blokyk <courvoisier.zoe@gmail.com>
2026-03-08 04:25:00 +0100

    figlet: use npins to manage figlet-fonts

--- programs/figlet.nix
+++ programs/figlet.nix
@@ -1,12 +1,4 @@
-{ pkgs, ... }:
-let
-  custom-fonts = pkgs.fetchFromGitHub {
-    owner = "xero";
-    repo = "figlet-fonts";
-    rev = "5c250192890856486be8a85085e7915b1b655f3e";
-    hash = "sha256-wT1DjM+3+UasAm2IHavBXs0R8eNMJn9uLtWSqwS+XU0=";
-  };
-in {
+{ pkgs, ... }: {
   nixpkgs.overlays = [
     (final: prev: {
       figlet = prev.figlet.overrideAttrs (final: prev: {
@@ -14,7 +6,7 @@ in {
           name = "fonts";
           paths = [
             prev.contributed
-            custom-fonts
+            <figlet-fonts>
           ];
         };
       });
