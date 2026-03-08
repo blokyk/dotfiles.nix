85293989a070006cee0fdfff46a3ddc6dfa9c68a
blokyk <courvoisier.zoe@gmail.com>
2026-03-08 04:24:09 +0100

    clock: migrate clock script

---
+++ scripts/clock.sh
@@ -0,0 +1,26 @@
+# shellcheck shell=bash
+
+export FONT=${1:-ANSI Shadow}
+
+function center_txt() {
+	txt="$(cat /dev/stdin)"
+	line_nb=$(echo "$txt" | wc -l)
+	available_lines=$(((LINES - line_nb) / 2))
+
+	for _ in $(seq $available_lines); do echo; done
+
+	echo "$txt"
+
+	for _ in $(seq $available_lines); do echo; done
+}
+
+function prnt() {
+	(
+		date "+%H : %M : %S" | figlet -C utf8 -ctf "$FONT";
+		date "+%a. %d %b %Y" | figlet -C utf8 -ctf Small
+	) | center_txt
+}
+
+export -f center_txt prnt
+
+watch -n 1 -x bash -c prnt
--- scripts/scripts.nix
+++ scripts/scripts.nix
@@ -13,65 +13,75 @@
   npins,
   ripgrep,
   xclip,
+  unixtools,
 
   writeShellApplication,
   ...
-}: rec {
+}:
+let
+  read = builtins.readFile;
+in rec {
+  clock = writeShellApplication {
+    name = "clock";
+    runtimeInputs = [ coreutils figlet unixtools.watch ];
+    text = read ./clock.sh;
+  };
+
   figlet-fzf = writeShellApplication {
     name = "figlet-fzf";
     runtimeInputs = [ figlet fzf xclip ];
     excludeShellChecks = [ "SC2016" "SC2034" ];
-    text = builtins.readFile ./figlet-fzf.sh;
+    text = read ./figlet-fzf.sh;
   };
 
   hm-diff = writeShellApplication {
     name = "hm-diff";
     runtimeInputs = [ coreutils gawk lix-diff /*home-manager*/ ];
-    text = builtins.readFile ./hm-diff.sh;
+    text = read ./hm-diff.sh;
   };
 
   hm-switch = writeShellApplication {
     name = "hm-switch";
     runtimeInputs = [ nix-output-monitor npins-shell ];
-    text = builtins.readFile ./hm-switch.sh;
+    text = read ./hm-switch.sh;
   };
 
   issue-fzf = writeShellApplication {
     name = "issue-fzf";
     runtimeInputs = [ coreutils fzf gh gnused /* $EDITOR */ ];
     excludeShellChecks = [ "SC2016" ];
-    text = builtins.readFile ./issue-fzf.sh;
+    text = read ./issue-fzf.sh;
   };
 
   nix-drv-out = writeShellApplication {
     name = "nix-drv-out";
     runtimeInputs = [ jq nix-impl-cli ];
-    text = builtins.readFile ./nix-drv-out.sh;
+    text = read ./nix-drv-out.sh;
   };
 
   nixp = writeShellApplication {
     name = "nixp";
     runtimeInputs = [ coreutils fzf nix-impl-cli /* $EDITOR */ ];
-    text = builtins.readFile ./nixp.sh;
+    text = read ./nixp.sh;
   };
 
   npins-shell = writeShellApplication {
     name = "npins-shell";
     runtimeInputs = [ coreutils npins ];
-    text = builtins.readFile ./npins-shell.sh;
+    text = read ./npins-shell.sh;
   };
 
   pr-fzf = writeShellApplication {
     name = "pr-fzf";
     runtimeInputs = [ coreutils fzf gh ];
     excludeShellChecks = [ "SC2016" ];
-    text = builtins.readFile ./pr-fzf.sh;
+    text = read ./pr-fzf.sh;
   };
 
   frg = writeShellApplication {
     name = "frg";
     runtimeInputs = [ bat fzf ripgrep ];
     excludeShellChecks = [ "SC2016" ];
-    text = builtins.readFile ./rg-fzf.sh;
+    text = read ./rg-fzf.sh;
   };
 }
