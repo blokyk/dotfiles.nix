8a423e4f94cd7d50972f4b87fdcbba70346798d7
blokyk <courvoisier.zoe@gmail.com>
2026-03-19 04:48:17 +0100

    pkgs: add imhex, pixieditor and sly

--- programs/pkgs/apps.nix
+++ programs/pkgs/apps.nix
@@ -4,8 +4,14 @@
     cutter
     # reverse-engineering tools, including an advanced decompiler
     ghidra
+    # very good hex editing gui
+    imhex
     # kindle comic converter
     kcc
+    # powerful 2d image editor with a node graph, mostly geared towards pixel art
+    pixieditor
+    # easy-to-use quick image editor
+    sly
     # note-taking and pdf annotation
     xournalpp
   ];
