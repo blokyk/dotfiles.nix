945e4501f5075a9a334aab72433bf620dd73980e
blokyk <courvoisier.zoe@gmail.com>
2026-03-30 17:32:59 +0200

    p10k: set theme to robbyrussell-ascii

--- zsh/p10k.nix
+++ zsh/p10k.nix
@@ -1,7 +1,10 @@
-{ ... }: {
+{ config, ... }: {
   imports = [ (import <zoeee/hm-modules>) ];
 
   programs.zsh-powerlevel10k = {
     enable = true;
+    theme = config.programs.zsh-powerlevel10k.themes.robbyrussell // {
+      mode = "ascii";
+    };
   };
 }
