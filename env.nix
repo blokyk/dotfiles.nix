aad3bd07f3a7408e975b96c0bbf18e63dfb9ae54
blokyk <courvoisier.zoe@gmail.com>
2026-03-30 16:44:44 +0200

    env: config XDG data dirs (share/)

--- env.nix
+++ env.nix
@@ -1,6 +1,21 @@
-{ config, ... }: {
+{ config, lib, ... }:
+let
+  HOME = config.home.homeDirectory;
+in {
   home.sessionVariables = {
     TERM = "xterm-256color";
     ANDROID_HOME = config.home.homeDirectory + "/Android/Sdk";
   };
+
+  xdg.systemDirs.data = lib.mkAfter [
+    "/usr/share/ubuntu"
+    "/usr/share/gnome"
+    "/usr/local/share"
+    "/usr/share"
+    "/var/lib/snapd/desktop"
+    "/var/lib/flatpak/exports/share"
+    "/nix/var/nix/profiles/default/share"
+    "${HOME}/.local/share/flatpak/exports/share"
+    "${HOME}/.nix-profile/share"
+  ];
 }
