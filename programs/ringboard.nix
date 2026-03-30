6d7662220e9b1aba7d9f35e1425295a4d040ee33
blokyk <courvoisier.zoe@gmail.com>
2026-03-30 16:47:53 +0200

    ringboard: don't auto-paste on select in egui

--- programs/ringboard.nix
+++ programs/ringboard.nix
@@ -1,8 +1,19 @@
 # A clipboard history manager
-{ ... }: {
+{ pkgs, ... }:
+let
+  toTOML = (pkgs.formats.toml { }).generate;
+in {
   imports = [ (import <zoeee/hm-modules>) ];
 
   services.ringboard = {
     x11.enable = true;
+  };
+
+  xdg.configFile = {
+    "ringboard/x11.toml".source = toTOML "ringboard-x11-config.toml" {
+      version = "V1";
+      auto_paste = false;
+      fast_path_optimzations = false; # allows ringboard to not keep passwords
+    };
   };
 }
