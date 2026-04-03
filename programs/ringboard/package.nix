e54b8de16897219ae39cd2aa0a0ab8dc49663710
blokyk <courvoisier.zoe@gmail.com>
2026-04-03 18:16:17 +0200

    ringboard: use pin for fenix dep

--- programs/ringboard/package.nix
+++ programs/ringboard/package.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, ... }:
 let
   # allows us to use nightly rust
-  fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") {};
+  fenix = import <fenix> {};
 in
 pkgs.ringboard.override {
   # incredibly cursed/hacky way to override buildRustPackage args,
