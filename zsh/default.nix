b011868bbacfbab33e9ace89942d95ae5b33fbd5
blokyk <courvoisier.zoe@gmail.com>
2026-04-16 12:06:30 +0200

    git: migrate a few zshoptions
    the biggest missing piece is still z4h, but this is about as much as i
    can do to make things closer to my old setup without *actually* using
    z4h

--- zsh/default.nix
+++ zsh/default.nix
@@ -16,5 +16,23 @@
     hooks = {
       chpwd = "__ls_after_cd";
     };
+
+    # https://zsh.sourceforge.io/Doc/Release/Options.html
+    setOptions = [
+      # allow cd-ing into a directory by just typing its name as a command
+      "auto_cd"
+
+      # add the timestamp in front of the command in the history file
+      "extended_history"
+
+      # don't expand !! & co
+      "no_bang_hist"
+
+      # make globs also match hidden files
+      "glob_dots"
+
+      # if a glob doesn't match any files, just leave it verbatim
+      "no_nomatch"
+    ];
   };
 }
