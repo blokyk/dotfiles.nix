0ffa212c90296d36ea0fa8305278155aef243044
blokyk <courvoisier.zoe@gmail.com>
2026-04-17 23:01:32 +0200

    less: ACTUALLY init (fucked up the clippaste like a dumbass)

--- programs/less.nix
+++ programs/less.nix
@@ -1,1 +1,38 @@
-https://github.com/blokyk/dotfiles.nix
+# The classic unix pager
+{ lib, ... }:
+let
+  options = {
+    # makes search ignore case *if* the query is all lowercase;
+    # if there is an uppercase letter, search is case-sensitive
+    ignore-case = true;
+
+    # display both line number and pourcentage of file in status bar
+    LONG-PROMPT = true;
+
+    # enable mouse support in less
+    mouse = true;
+
+    # makes less display ANSI color sequences correctly instead of escaping them
+    RAW-CONTROL-CHARS = true;
+
+    # makes less automatically quit if the entire input can be displayed without paging
+    quit-if-one-screen = true;
+
+    # size of tab stops
+    tabs = 4;
+  };
+
+  optToFlag = name: val:
+    if val == true then
+      "--${name}"
+    else
+      "--${name}=${toString val}";
+in {
+  programs.less = {
+    enable = true;
+    inherit options;
+  };
+
+  home.sessionVariables.LESS =
+    lib.concatMapAttrsStringSep " " optToFlag options;
+}
