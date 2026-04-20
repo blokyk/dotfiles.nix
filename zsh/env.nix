c28ab83139232265faa53b5a51d41434ce9eb920
blokyk <courvoisier.zoe@gmail.com>
2026-04-20 11:50:23 +0200

    env: add system-manager paths to PATH

--- zsh/env.nix
+++ zsh/env.nix
@@ -2,6 +2,11 @@
   home.sessionVariables = {
   };
 
+  home.sessionPath = [
+    "/run/wrappers/bin" # wrappers have a higher priority (i.e. override) system binaries
+    "/run/system-manager/sw/bin"
+  ];
+
   programs.zsh.envExtra = ''
     export XDG_DATA_DIRS="${lib.concatStringsSep ":" config.xdg.systemDirs.data}"
   '';
