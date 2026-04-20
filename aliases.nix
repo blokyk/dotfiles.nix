1d376bc0a63b2be2ae94f2969b9ff0db8ee1b3ab
blokyk <courvoisier.zoe@gmail.com>
2026-04-20 07:43:13 +0200

    zsh: migrate corrupted aliases

--- aliases.nix
+++ aliases.nix
@@ -1,16 +1,27 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, scripts, ... }:
 let
   mktemp = lib.getExe' pkgs.coreutils "mktemp";
   xclip  = lib.getExe  pkgs.xclip;
+  csharprepl = lib.getExe pkgs.csharprepl;
+  gnome-text-editor = lib.getExe pkgs.gnome-text-editor;
+  syno = lib.getExe scripts.syno;
 in {
   home.shellAliases = {
-    ll = "ls -lha";
     la = "ls -A";
-    ip = "ip -c";
+    ll = "ls -lha";
+    lspci = "lspci -tv";
+    ip = "ip -c"; # always use color
+    ssh = "ssh -C"; # always use compression
     tree = "tree -a -I .git";
 
     cdtmp = "cd $(${mktemp} -d)";
     clipcopy = "${xclip} -sel clip";
     clippaste = "${xclip} -sel clip -o";
+
+    wdf = "watch -n .5 df -h";
+    syno-fr = "LANG=fr ${syno}";
+
+    csharp = csharprepl;
+    gedit = gnome-text-editor;
   };
 }
