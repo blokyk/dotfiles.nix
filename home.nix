946f6ff3128b44b079c45eecca4bd14daee8ba58
blokyk <courvoisier.zoe@gmail.com>
2026-04-23 16:26:25 +0200

    nit: fix home-manager news

--- home.nix
+++ home.nix
@@ -1,5 +1,6 @@
 { lib, ... }:
 let config = import ./default.nix; in {
+  news = lib.mkForce config.config.news;
   home = {
     inherit (config.config.home) homeDirectory stateVersion username;
     activationPackage = lib.mkForce config.config.home.activationPackage;
