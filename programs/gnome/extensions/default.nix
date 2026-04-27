4fece3fdd9122a2761fc95dfc17e9ee4a02b809c
blokyk <courvoisier.zoe@gmail.com>
2026-04-27 18:15:10 +0200

    gnome(ext): disable desktop icons by default

--- programs/gnome/extensions/default.nix
+++ programs/gnome/extensions/default.nix
@@ -2,4 +2,10 @@
   imports = [(
     lib.modules.importApply <self/misc/importNixFilesAndDirs.nix> ./.
   )];
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      disabled-extensions = [ "ding@rastersoft.com" ];
+    };
+  };
 }
