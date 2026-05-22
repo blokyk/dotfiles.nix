7e1c807172674e94b473da6f79d60fd7aa28219f
blokyk <courvoisier.zoe@gmail.com>
2026-05-22 10:15:42 +0200

    nano: add nix syntax highlighting

--- programs/nano/nanorc-pkg.nix
+++ programs/nano/nanorc-pkg.nix
@@ -2,6 +2,13 @@
   nanorc,
   ...
 }:
-nanorc.overrideAttrs {
+let
+  nix-nanorc = <nanonix/nix.nanorc>;
+in
+nanorc.overrideAttrs (prev: {
+  postUnpack = (prev.postUnpack or "") + ''
+    cp ${nix-nanorc} ''$sourceRoot/nix.nanorc
+  '';
+
   patches = [ ./git-commit-highlight-long-message.patch ];
-}
+})
