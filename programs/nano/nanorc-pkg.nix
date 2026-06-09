a21d613a1d7dfb09b08d8e9e80596601e4de57c0
blokyk <courvoisier.zoe@gmail.com>
2026-06-09 14:32:35 +0200

    nano: use galenguyer/nano-syntax-highlighting for nanorc
    found this trying to set TAB to \t in makefiles :)

--- programs/nano/nanorc-pkg.nix
+++ programs/nano/nanorc-pkg.nix
@@ -1,4 +1,5 @@
 {
+  fetchFromGitHub,
   nanorc,
   ...
 }:
@@ -6,6 +7,13 @@ let
   nix-nanorc = <nanonix/nix.nanorc>;
 in
 nanorc.overrideAttrs (prev: {
+  src = fetchFromGitHub {
+    owner = "galenguyer";
+    repo = "nano-syntax-highlighting";
+    rev = "a1df5b66aabe9048b13b58beec1e61abbd729c77";
+    hash = "sha256-tcRNoeg0j/z9wFZjIc1CJXOKieWrvlLq9pblB0kE6yc=";
+  };
+
   postUnpack = (prev.postUnpack or "") + ''
     cp ${nix-nanorc} ''$sourceRoot/nix.nanorc
   '';
