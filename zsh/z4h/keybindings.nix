eb288d79b5f45d67d9c9f135ae078bfffd271fc4
blokyk <courvoisier.zoe@gmail.com>
2026-06-10 02:04:26 +0200

    z8h: use custom keybinding module maybe?

--- zsh/z4h/keybindings.nix
+++ zsh/z4h/keybindings.nix
@@ -4,10 +4,17 @@ let
 
   fn = import ./fn/list.nix;
 in {
+  imports = [(
+    lib.modules.importApply <zoeee/hm-modules/mk-keybindings> {
+      optPath = [ "programs" "z3h" "keybindings" ];
+      prefixPath = [ "programs" "zsh" "initBlocks" "keybindings" ];
+      setter = action: keybind: ''
+        bindkey ${action}
+      '';
+    }
+  )];
+
   options = {
-    programs.z8h.keybindings = {
-      
-    };
   };
 
   config = {
