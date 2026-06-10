f1a06b36b5eecd01dcac44ff16ddb79b1dd66013
blokyk <courvoisier.zoe@gmail.com>
2026-06-10 12:31:22 +0200

    zsh: use programs.zsh.history for history-related options

--- zsh/default.nix
+++ zsh/default.nix
@@ -14,23 +14,23 @@
       chpwd = "__ls_after_cd";
     };
 
+    history = {
+      # add the timestamp in front of the command in the history file
+      extended = true;
+      # do not reread commands from the history file after the
+      # shell session has started
+      share = false;
+      # delete old recorded entry if new entry is a duplicate
+      ignoreAllDups = true;
+    };
+
     # https://zsh.sourceforge.io/Doc/Release/Options.html
     setOptions = [
       # allow cd-ing into a directory by just typing its name as a command
       "auto_cd"
 
-      # add the timestamp in front of the command in the history file
-      "extended_history"
-
       # don't expand !! & co
       "no_bang_hist"
-
-      # do not reread commands from the history file after the
-      # shell session has started
-      "no_share_history"
-
-      # delete old recorded entry if new entry is a duplicate
-      "hist_ignore_all_dups"
 
       # record how much time the command took to finish,
       # and append it immediately to the history file
