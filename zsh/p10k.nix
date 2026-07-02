388e4fb393da32d19fd6a53baf4afe9bc08b18ae
blokyk <courvoisier.zoe@gmail.com>
2026-07-02 18:06:27 +0200

    zsh(p10k): change prompt if in nix shell

--- zsh/p10k.nix
+++ zsh/p10k.nix
@@ -1,39 +1,45 @@
-{ config, ... }: {
+{ config, lib, ... }: {
   programs.zsh-powerlevel10k = {
     enable = true;
-    theme = config.programs.zsh-powerlevel10k.themes.robbyrussell // {
-      mode = "ascii";
+    theme = lib.mkMerge [
+      config.programs.zsh-powerlevel10k.themes.robbyrussell
+      {
+        mode = "compatible";
 
-      # this is the default robbyrussell git indicator, but with an
-      # added pink '*' next to the branch if there are any changes
-      # in the stash
-      vcs.formatter = ''
-        emulate -L zsh
-        if [[ -n $P9K_CONTENT ]]; then
-          # If P9K_CONTENT is not empty, it's either "loading" or from vcs_info (not from
-          # gitstatus plugin). VCS_STATUS_* parameters are not available in this case.
-          typeset -g my_git_format=$P9K_CONTENT
-        else
-          # Use VCS_STATUS_* parameters to assemble Git status. See reference:
-          # https://github.com/romkatv/gitstatus/blob/master/gitstatus.plugin.zsh.
+        prompt-char.symbol = lib.mkForce "\${\${NIX_SHELL_LEVEL:+%Bλ%b}:-%B➜%b }";
 
-          # bold purple 'git:(' prefix
-          typeset -g my_git_format="''${1+%B%4F}git:(''${1+%1F}"
+        # this is the default robbyrussell git indicator, but with an
+        # added pink '*' before the status if there are any changes
+        # in the stash
+        vcs.formatter = lib.mkForce ''
+          emulate -L zsh
+          if [[ -n $P9K_CONTENT ]]; then
+            # If P9K_CONTENT is not empty, it's either "loading" or from vcs_info (not from
+            # gitstatus plugin). VCS_STATUS_* parameters are not available in this case.
+            typeset -g my_git_format=$P9K_CONTENT
+          else
+            # Use VCS_STATUS_* parameters to assemble Git status. See reference:
+            # https://github.com/romkatv/gitstatus/blob/master/gitstatus.plugin.zsh.
 
-          # either the branch name, or the commit if detached
-          my_git_format+=''${''${VCS_STATUS_LOCAL_BRANCH:-''${VCS_STATUS_COMMIT[1,8]}}//\%/%%}
+            # bold purple 'git:(' prefix
+            typeset -g my_git_format="''${1+%B%4F}git:(''${1+%1F}"
 
-          # if there are things in the stash, add a pink star after the branch name
-          if (( VCS_STATUS_STASHES > 0 )); then
-            my_git_format+="''${1+%13F}*"
+            # either the branch name, or the commit if detached
+            my_git_format+=''${''${VCS_STATUS_LOCAL_BRANCH:-''${VCS_STATUS_COMMIT[1,8]}}//\%/%%}
+            my_git_format+="''${1+%4F})"
+
+            # if there are things in the stash, add a pink star before the status
+            if (( VCS_STATUS_STASHES > 0 )); then
+              my_git_format+="''${1+%13F}*"
+            fi
+
+            if (( VCS_STATUS_NUM_CONFLICTED || VCS_STATUS_NUM_STAGED ||
+                  VCS_STATUS_NUM_UNSTAGED   || VCS_STATUS_NUM_UNTRACKED )); then
+              my_git_format+=" ''${1+%3F}✗"
+            fi
           fi
-          my_git_format+="''${1+%4F})"
-          if (( VCS_STATUS_NUM_CONFLICTED || VCS_STATUS_NUM_STAGED ||
-                VCS_STATUS_NUM_UNSTAGED   || VCS_STATUS_NUM_UNTRACKED )); then
-            my_git_format+=" ''${1+%3F}✗"
-          fi
-        fi
-      '';
-    };
+        '';
+      }
+    ];
   };
 }
