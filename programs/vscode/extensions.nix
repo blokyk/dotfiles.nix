482b101c5387131b8ca56d6564ffa37f4f13b549
blokyk <courvoisier.zoe@gmail.com>
2026-07-09 02:04:38 +0200

    vscode: move extensions to separate file

---
+++ programs/vscode/extensions.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+let
+  nixpkgsExtensions = with pkgs.vscode-extensions; [
+    # suggestions and go-to for path-like things, mainly files and folders
+    christian-kohler.path-intellisense
+    # nixd-compatible extension
+    jnoortheen.nix-ide
+    # clangd, a c++ lsp
+    (pkgs.callPackage ./wrapped-clangd.nix {})
+    # python syntax highlighting + language server
+    ms-python.python
+    ms-python.vscode-pylance
+    # bash script checking
+    timonwong.shellcheck
+
+    # finds 'todo:' and 'fixme:' comments and displays them by-file
+    gruntfuggly.todo-tree
+    # highlight trailing spaces at the end of lines
+    shardulm94.trailing-spaces
+    # code-spell code and comments
+    streetsidesoftware.code-spell-checker
+    streetsidesoftware.code-spell-checker-french
+
+    # one dark pro theme
+    zhuangtongfa.material-theme
+  ];
+
+  marketplaceExtensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    # remote ssh stuff
+    { name = "remote-ssh"; publisher = "ms-vscode-remote"; version = "0.125.2026062315"; sha256 = "hiqvNbcVQBOJ/x4mjQHlKp76s/B3GGvxuy6LGi1OKKs=";  }
+    { name = "remote-ssh-edit"; publisher = "ms-vscode-remote"; version = "0.87.0"; sha256 = "1qqsnzn9z11jr72n7cl0ab6i9mv49c0ijcp699zbglv5092gmrf9";  }
+    { name = "remote-explorer"; publisher = "ms-vscode"; version = "0.6.2026031809"; sha256 = "WRg8ObPVQMluuCW/dBM2ibBDW/zc8cQQS5QQMfcpw2c=";  }
+
+    # add a unique accent color to each window based on workspace path
+    { name = "unique-window-colors"; publisher = "stuart"; version = "1.2.9"; sha256 = "0lbjnihxaznm2r6zh6a1mz96h89430ba6cr4k43qhpk2gpk3d2vf";  }
+
+    # old icons from before <=1.105
+    { name = "vscode-oldicons"; publisher = "adam-bender";  version = "0.2.0"; sha256 = "0p7k0safirn9bf459d5m77igdx8qd6b3rs8j0ndgjyvjlw54wkhs";  }
+    # normal vscode icons w/ folder icons instead of '>'
+    { name = "seti-minimal-folder"; publisher = "sabaken";  version = "1.0.3"; sha256 = "0ip528v3jpbyalpimw4myddmxfvr51bs5104lx5anad9icm37pm4";  }
+    { name = "vscode-theme-onelight"; publisher = "akamud"; version = "2.3.0"; sha256 = "sha256-CTD0s2lRMCi/WCGr6dP1Utrvtsdcbg4srRcrZJSFDqU="; }
+  ];
+in {
+  programs.vscode.profiles.default.extensions = nixpkgsExtensions ++ marketplaceExtensions;
+}
