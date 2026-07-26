2c0ac6ff254fedaaf0b3e5ba193e4b7b7f55e6b2
blokyk <courvoisier.zoe@gmail.com>
2026-07-26 14:22:21 +0200

    nit: cleanup graphical apps list

--- programs/pkgs/apps.nix
+++ programs/pkgs/apps.nix
@@ -1,20 +1,17 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    # desktop client for matrix
-    # fixme: broken #element-desktop
-    # reverse-engineering tools, including an advanced decompiler
-    ghidra
-    # alternative gnome shell extension managing GUI
-    # fixme: broken #gnome-extension-manager
     # very good hex editing gui
     imhex
-    # open-source linux alternative to synthesia, the midi visualizer
-    # fixme: no sound #neothesia
-    # powerful 2d image editor with a node graph, mostly geared towards pixel art
-    pixieditor
     # easy-to-use quick image editor
     sly
-    # note-taking and pdf annotation
-    xournalpp
   ];
+
+  # broken:
+  #   - element-desktop
+  #   - gnome-extension-manager
+  #   - neothesia (no sound)
+  # removed:
+  #   - ghidra
+  #   - pixieditor
+  #   - xournalpp
 }
