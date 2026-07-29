483688e89a58786f6356759c56482e33d9e2cc37
blokyk <courvoisier.zoe@gmail.com>
2026-07-29 18:21:21 +0200

    fix: reload sessionVariables on every new shell
    this shit is why i had a mysteriously stale NIX_PATH... -_-

--- env.nix
+++ env.nix
@@ -2,6 +2,20 @@
 let
   HOME = config.home.homeDirectory;
 in {
+  # ! this is bad but it's not my fault home-manager is bad
+  home.file."${lib.removePrefix "${HOME}/" config.programs.zsh.dotDir}/.zprofile".text = lib.mkBefore ''
+    # make sure HM session variables are always sourced,
+    # not just on graphical environment start
+    unset __HM_SESS_VARS_SOURCED
+    unset __HM_ZSH_SESS_VARS_SOURCED
+  '';
+  home.file."${lib.removePrefix "${HOME}/" config.programs.zsh.dotDir}/.zshenv".text = lib.mkBefore ''
+    # make sure HM session variables are always sourced,
+    # not just on graphical environment start
+    unset __HM_SESS_VARS_SOURCED
+    unset __HM_ZSH_SESS_VARS_SOURCED
+  '';
+
   home.sessionVariables = {
     TERM = "xterm-256color";
     ANDROID_HOME = config.home.homeDirectory + "/Android/Sdk";
