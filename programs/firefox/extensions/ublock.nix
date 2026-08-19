59e5b54a72fec6e18b6c6f3e1ee485cc2dfa5ac5
blokyk <courvoisier.zoe@gmail.com>
2026-08-20 01:26:10 +0200

    firefox: add new ublock filters

--- programs/firefox/extensions/ublock.nix
+++ programs/firefox/extensions/ublock.nix
@@ -7,15 +7,44 @@
       force = true;
       settings = {
         selectedFilterLists = [
+          # built-in
           "ublock-filters"
           "ublock-badware"
           "ublock-privacy"
           "ublock-unbreak"
           "ublock-quick-fixes"
+          "ublock-unbreak"
+
+          # ads
           "easylist"
+          "adguard-generic"
+          "adguard-mobile"
+
+          # privacy
           "easyprivacy"
+          "adguard-spyware-url"
+
+          # malware/phishing protection
           "urlhaus-1"
-          "plowe-0"
+          "curben-phishing"
+
+          # cookie notices
+          "fanboy-cookiemonster"
+          "ublock-cookies-easylist"
+          "adguard-cookies"
+          "ublock-cookies-adguard"
+
+          # annoyances
+          "fanboy-ai-suggestions"
+          "easylist-chat"
+          "easylist-newsletters"
+          "easylist-notifications"
+          "easylist-annoyances"
+          "ublock-annoyances"
+
+          # misc
+          "plowe-0" # Peter Lowe’s Ad and tracking server list
+          "user-filters" # the custom 'My filters' list
         ];
 
         user-filters = ''
