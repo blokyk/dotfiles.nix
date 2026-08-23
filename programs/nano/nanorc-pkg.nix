14aafe88459899be3285f92c2b9f443e66fcdf29
blokyk <courvoisier.zoe@gmail.com>
2026-08-24 01:32:19 +0200

    chore: move a few sources to npins

--- programs/nano/nanorc-pkg.nix
+++ programs/nano/nanorc-pkg.nix
@@ -7,12 +7,7 @@ let
   nix-nanorc = <nanonix/nix.nanorc>;
 in
 nanorc.overrideAttrs (prev: {
-  src = fetchFromGitHub {
-    owner = "galenguyer";
-    repo = "nano-syntax-highlighting";
-    rev = "a1df5b66aabe9048b13b58beec1e61abbd729c77";
-    hash = "sha256-tcRNoeg0j/z9wFZjIc1CJXOKieWrvlLq9pblB0kE6yc=";
-  };
+  src = <nano-syntax-highlighting>;
 
   postUnpack = (prev.postUnpack or "") + ''
     cp ${nix-nanorc} ''$sourceRoot/nix.nanorc
--- scripts/syno/en-thes.nix
+++ scripts/syno/en-thes.nix
@@ -1,11 +1,6 @@
-{ fetchFromGitHub, runCommand }:
+{ runCommand }:
 runCommand "en-thes" {
-  src = fetchFromGitHub {
-    owner = "hunspell";
-    repo = "mythes";
-    rev = "e43e32167d8cbcd94eef36895b39706946ce1479";
-    hash = "sha256-m/pEpiktnFbOW7Ms1vkzVecCPLVi+gdCWI0MgPn8wTQ=";
-  };
+  src = <thesauruses>;
 } ''
   mkdir $out
   cp $src/th_en_US_new.dat $out/thes_en.dat
